44090f480ba00d48fde2f6738b7c1f3471235fd4
Aishwarya_Birajdar <aishbirajdar2306@gmail.com>
2025-08-26 11:53:59 +0530

    Create Day12.java
    12th dayyyy

---
+++ Day12.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') st.push(')');
+            else if (c == '{') st.push('}');
+            else if (c == '[') st.push(']');
+            else {
+                if (st.isEmpty() || st.pop() != c) return false;
+            }
+        }
+        return st.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));     
+        System.out.println(isValid("([)]"));   
+        System.out.println(isValid("[{()}]")); 
+        System.out.println(isValid(""));       
+        System.out.println(isValid("{[}"));    
+    }
+}
